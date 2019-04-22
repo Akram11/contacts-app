e5d317a9e846c372e29dc8c96edc8b58e897c412
Akram A <akram.f11@gmail.com>
2019-04-22 18:55:12 +0200

    added a function to remove a contact

--- src/App.js
+++ src/App.js
@@ -18,7 +18,14 @@ class App extends Component {
       })
   }
 
-
+  createContact = (contact) => {
+    ContactsAPI.create(contact)
+      .then((contact) => {
+        this.setState((currentState) => ({
+          contacts: currentState.contacts.concat([contact])
+        }))
+      })
+  }
 
   removeContact = (contact) => {
     this.setState((currentState)=>({
@@ -40,10 +47,14 @@ class App extends Component {
                     />
           
       )} />
-        <Route path='/create' render= {() => (
-          <CreateContact />
-        )}
-         />
+        <Route path='/create' render={({ history }) => (
+          <CreateContact
+            onCreateContact={(contact) => {
+              this.createContact(contact)
+              history.push('/')
+            }}
+          />
+        )} />
       </div>
     );
   }
